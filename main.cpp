649a6282149717ae2c5b307153913c22eb00b6d3
Alyssa <narue1992@yahoo.com>
2016-10-07 18:42:28 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.cpp
+ * Author: thaoc
+ *
+ * Created on May 20, 2015, 8:02 PM
+ */
+
+#include <cstdlib>
+#include "ZombieWar.h"
+
+
+int main(int argc, char** argv) {
+    ZombieWar* zw = new ZombieWar();
+    zw->start();
+}
