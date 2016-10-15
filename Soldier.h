320cb92cdadc81c7420ca64bd7312625f20f7754
Alyssa <narue1992@yahoo.com>
2016-10-15 15:10:32 -0500

    Update Soldier.h

--- Soldier.h
+++ Soldier.h
@@ -1,17 +1,27 @@
 #ifndef SOLDIER_H
 #define SOILDER_H
-class Soldier {
-  public:
-    Soldier(){health = 100; attack = 10;}
-    Soldier(int hp, int atk);
-    ~Soldier();
 
-    int getHealth(){return health;}
-    int getAttack(){return attack;}
-    void updateHealth(int damage){health -= damage;}
-  private:
-    int health;
-    int attack;
+class IZombie;
+
+class Soldier{
+
+private:
+        int health; //variable to hold health
+        int at; //variable to store attack strength
+
+public:
+        Soldier(){health = 100; at = 10;} //Constructor to initialize a child
+        int getHealth(){return health;} //Accessor to return the health
+        void decreaseHealth(int damage){health -= damage;} //Mutator to update health when attacked
+        int getAttack(){return at;} //Accessor to return the attack strength
+
+
+        virtual void attack(IZombie * zombie){
+                zombie -> decreaseHealth(getAttack());
+        }
+
 };
 
 #endif
+
+
