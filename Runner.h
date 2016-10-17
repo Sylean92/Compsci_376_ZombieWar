d841802f92f876b7880ce9c62bfe28a53faf2a38
cstolps8 <stolpacj08@uww.edu>
2016-10-16 20:36:02 -0500

    Update Runner.h

--- Runner.h
+++ Runner.h
@@ -7,44 +7,28 @@
 
 #ifndef RUNNER_H
 #define	RUNNER_H
+#include "ISurvivor.h"
+#include "IZombie.h"
 
- class Runner {
-   public:
-     Runner();
-     ~Runner();
+ class ISurvivor;
 
-     double getRunnerHealth();
-     double getRunnerAttack();
-     void setRunnerHealth(double newHp);
+class Runner {
+ 
+	private:
+		int health;
+		int at;
+	public:
 
-   private:
-     double health;
-     double attack;
- };
+        Runner(){health = 5; at = 8;} //Constructor to initialize a runner zombie
+        int getHealth(){return health;} //Accessor to return the health
+        void decreaseHealth(int damage){health -= damage;} //Mutator to update health when attacked
+        int getAttack(){return at;} //Accessor to return the attack strength
 
-  Runner :: Runner(){
 
-        health=5;
-        attack=30;
-  }
+        virtual void attack(IZombie * zombie){
+                zombie -> decreaseHealth(getAttack());
+        }
 
-  double Runner:: getRunnerAttack(){
-
-        return attack;
-
-  }
-
-  double Runner:: getRunnerHealth(){
-
-        return health;
-
-  }
-
-  void Runner:: setRunnerHealth(double attack){
-
-        health = health-attack;
-
-  }
+};
 
 #endif	/* RUNNER_H */
-
